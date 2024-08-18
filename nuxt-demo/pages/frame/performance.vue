a90046cf58b3e5a6f6c4204fd960cddcb761c269
WanPeng <986063761@qq.com>
2024-08-18 20:30:58 +0800

    更新

--- nuxt-demo/pages/frame/performance.vue
+++ nuxt-demo/pages/frame/performance.vue
@@ -3,6 +3,12 @@
     <div v-for="({ label, text }) in timings" :key="label">
       {{ label }}: {{ text }}
     </div>
+
+    <el-button type="primary">触发 window.onerror 错误</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
   </div>
 </template>
 
