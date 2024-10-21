1dc401331fcaa9d32c41bf9c778a039ff7f19f4c
WanPeng <986063761@qq.com>
2024-10-21 16:25:12 +0800

    更新

--- vue2-pc-demo/tests/unit/commentInput.spec.js
+++ vue2-pc-demo/tests/unit/commentInput.spec.js
@@ -6,16 +6,43 @@ import Element from 'element-ui'
 Vue.use(Element)
 
 describe(Component.name, () => {
-  const wrapper = mount(Component)
-
-  it('是否存在 button', () => {
-    const button = wrapper.find('button')
-    expect(button.exists()).toBe(true)
+  const warningMock = jest.fn()
+  const wrapper = mount(Component, {
+    mocks: {
+      $message: {
+        warning: warningMock,
+      },
+    },
   })
 
-  it('模拟点击 button', async () => {
-    const button = wrapper.find('.btn-submit')
-    await button.trigger('click')
+  it('是否存在提交按钮', async () => {
+    const button = wrapper.findComponent('.btn-submit')
+    // 测试按钮是否存在
     expect(button.exists()).toBe(true)
+    
+    // 测试点击后是否有提醒内容
+    await button.trigger('click')
+    expect(warningMock).toHaveBeenCalledWith('请输入内容')
+  })
+})
+
+describe(Component.name + '提交按钮点击通过', () => {
+  const warningMock = jest.fn()
+  const wrapper = mount(Component, {
+    mocks: {
+      $message: {
+        warning: warningMock
+      }
+    }
+  })
+
+  it('有内容后，再点提交是否成功', async () => {
+    const button = wrapper.findComponent('.btn-submit')
+    
+    // 测试有内容后，再点击是否还有提醒
+    const input = wrapper.findComponent({ ref: 'input' })
+    input.element.innerHTML = '我输入了内容'
+    await button.trigger('click')
+    expect(warningMock).not.toHaveBeenCalled()
   })
 })
