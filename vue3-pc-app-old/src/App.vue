8a36d99eac48da35718971fb042c4d055795e910
WanPeng <986063761@qq.com>
2026-05-29 12:07:34 +0800

    更新

---
+++ vue3-pc-app-old/src/App.vue
@@ -0,0 +1,148 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { 
+  DataAnalysis,
+  Setting,
+  User,
+  Document,
+  MagicStick,
+  Shop
+} from '@element-plus/icons-vue'
+
+const router = useRouter()
+const route = useRoute()
+
+// 菜单折叠状态
+const isCollapse = ref(false)
+
+// 菜单数据
+const menuItems = [
+  {
+    index: '/data',
+    title: '数据渲染',
+    icon: DataAnalysis
+  },
+  {
+    index: '/component',
+    title: '组件 API 演示',
+    icon: User
+  },
+  {
+    index: '/builtin-components',
+    title: '内置组件演示',
+    icon: MagicStick
+  },
+  {
+    index: '/store-demo',
+    title: 'Store 演示',
+    icon: Shop
+  }
+]
+
+// 处理菜单点击
+const handleMenuClick = (index) => {
+  router.push(index)
+}
+
+// 切换菜单折叠状态
+const toggleCollapse = () => {
+  isCollapse.value = !isCollapse.value
+}
+</script>
+
+<template>
+  <!-- 左侧菜单 -->
+  <el-aside :width="isCollapse ? '64px' : '200px'" class="sidebar">
+    <div class="logo">
+      <h3 v-if="!isCollapse">Vue3 PC App</h3>
+      <h3 v-else>V3</h3>
+    </div>
+    
+    <el-menu
+      :default-active="route.path"
+      :collapse="isCollapse"
+      :unique-opened="true"
+      class="sidebar-menu"
+      @select="handleMenuClick"
+    >
+      <el-menu-item 
+        v-for="item in menuItems" 
+        :key="item.index"
+        :index="item.index"
+      >
+        <el-icon><component :is="item.icon" /></el-icon>
+        <template #title>{{ item.title }}</template>
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
+
+  <!-- 右侧内容区域 -->
+  <el-container class="app-main">
+    <router-view class="app-main-view"/>
+  </el-container>
+</template>
+
+<style lang="scss" scoped>
+.sidebar {
+  background-color: #304156;
+  transition: width 0.3s;
+}
+
+.app-main {
+
+  &-view {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+  }
+}
+
+.logo {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #2b3a4b;
+  color: white;
+  margin-bottom: 0;
+}
+
+.logo h3 {
+  margin: 0;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.sidebar-menu {
+  border: none;
+  background-color: #304156;
+}
+
+.sidebar-menu .el-menu-item {
+  color: #bfcbd9;
+}
+
+.sidebar-menu .el-menu-item:hover {
+  background-color: #263445;
+  color: #fff;
+}
+
+.sidebar-menu .el-menu-item.is-active {
+  background-color: #409eff;
+  color: #fff;
+}
+
+
+
+/* 响应式设计 */
+@media (max-width: 768px) {
+  .sidebar {
+    width: 64px !important;
+  }
+  
+  .logo h3 {
+    display: none;
+  }
+}
+</style>
--- vue3-pc-app/README.md
+++ vue3-pc-app/README.md
@@ -1,4 +1,4 @@
-# vue-project
+# vue-app
 
 This template should help get you started developing with Vue 3 in Vite.
 
@@ -9,11 +9,15 @@ This template should help get you started developing with Vue 3 in Vite.
 ## Recommended Browser Setup
 
 - Chromium-based browsers (Chrome, Edge, Brave, etc.):
-  - [Vue.js devtools](https://chromewebstore.google.com/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd) 
+  - [Vue.js devtools](https://chromewebstore.google.com/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd)
   - [Turn on Custom Object Formatter in Chrome DevTools](http://bit.ly/object-formatters)
 - Firefox:
   - [Vue.js devtools](https://addons.mozilla.org/en-US/firefox/addon/vue-js-devtools/)
   - [Turn on Custom Object Formatter in Firefox DevTools](https://fxdx.dev/firefox-devtools-custom-object-formatters/)
+
+## Type Support for `.vue` Imports in TS
+
+TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
 
 ## Customize configuration
 
@@ -31,7 +35,7 @@ npm install
 npm run dev
 ```
 
-### Compile and Minify for Production
+### Type-Check, Compile and Minify for Production
 
 ```sh
 npm run build
@@ -41,4 +45,29 @@ npm run build
 
 ```sh
 npm run test:unit
+```
+
+### Run End-to-End Tests with [Playwright](https://playwright.dev)
+
+```sh
+# Install browsers for the first run
+npx playwright install
+
+# When testing on CI, must build the project first
+npm run build
+
+# Runs the end-to-end tests
+npm run test:e2e
+# Runs the tests only on Chromium
+npm run test:e2e -- --project=chromium
+# Runs the tests of a specific file
+npm run test:e2e -- tests/example.spec.ts
+# Runs the tests in debug mode
+npm run test:e2e -- --debug
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
 ```
