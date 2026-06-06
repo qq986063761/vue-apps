52244a35f4838d6b733daeb9b145b6a85be2cc26
wanpeng <986063761@qq.com>
2026-06-06 16:11:54 +0800

    更新

--- federation-vue3-apps/main/vite.config.ts
+++ federation-vue3-apps/main/vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
         pinia: { singleton: true },
         'element-plus': { singleton: true },
       },
+      shareStrategy: 'loaded-first',
       dev: {
         remoteHmr: true,
       },
